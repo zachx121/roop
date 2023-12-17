e2c9722bf011e32d4e7dfd3806214abf34a5776e
Zachary4biz <zachary4biz@yeah.net>
2023-12-17 15:23:28 +0800

    add dropdown

--- run_gradio.py
+++ run_gradio.py
@@ -6,35 +6,36 @@ from PIL import Image
 from datetime import datetime
 
 NUM_THREADS_VIDEO = 20  # 8线程大概7.7G
+os.system("mkdir -p heads")
+os.system("mkdir -p outputs")
 
 # 定义处理函数
-def process_images(head_image, target_image_fp, use_enhancer):
-    # 保存上传的图片
-    Image.fromarray(head_image).save("head.jpeg")
-    # Image.fromarray(target_image).save("target.jpeg")
-    output_fname = ".".join(os.path.basename(target_image_fp).split(".")[:-1]) + "_swap.jpeg"
-
-    # 执行图像处理命令
+def process_images(head_image_fp, target_image_fp, use_enhancer):
+    print("head_image_fp is %s" % head_image_fp)
+    print("target_image_fp is %s" % target_image_fp)
     processor = "face_swapper face_enhancer" if use_enhancer else "face_swapper"
-    output_fname = "output_%s.jpeg" % datetime.now().strftime("%Y%m%d%H%M%S")
+    prefix = os.path.basename(head_image_fp).split(".")[0]
+    output_fname = f"{prefix}_" + ".".join(os.path.basename(target_image_fp).split(".")[:-1]) + "_swap.jpeg"
     command = "python run.py --execution-provider cuda " \
-              f"-s head.jpeg -t {target_image_fp} -o {output_fname} " \
+              f"-s {head_image_fp} " \
+              f"-t {target_image_fp} " \
+              f"-o {output_fname} " \
               "--frame-processor %s" % processor
     subprocess.run(command, shell=True)
 
     # 返回处理后的图像
-    processed_image = np.array(Image.open(output_fname))
-    return processed_image
+    return output_fname
+
 
 # python run.py --execution-provider cuda -s head.jpeg -t target.mp4 -o output.mp4 --frame-processor face_swapper --execution-threads 20
-def process_videos(head_image, target_video_fp, use_enhancer):
-    Image.fromarray(head_image).save("head.jpeg")
+def process_videos(head_image_fp, target_video_fp, use_enhancer):
+    print("head_image_fp is %s" % head_image_fp)
     print("target_video_fp is %s" % target_video_fp)
     processor = "face_swapper face_enhancer" if use_enhancer else "face_swapper"
-    # output_fname = "output_%s.mp4" % datetime.now().strftime("%Y%m%d%H%M%S")
-    output_fname = ".".join(os.path.basename(target_video_fp).split(".")[:-1]) + "_swap.mp4"
+    prefix = os.path.basename(head_image_fp).split(".")[0]
+    output_fname = f"{prefix}_" + ".".join(os.path.basename(target_video_fp).split(".")[:-1]) + "_swap.mp4"
     command = "python run.py --execution-provider cuda " \
-              "-s head.jpeg " \
+              f"-s {head_image_fp} " \
               f"-t {target_video_fp} " \
               f"-o {output_fname} " \
               f"--frame-processor {processor} " \
@@ -45,30 +46,45 @@ def process_videos(head_image, target_video_fp, use_enhancer):
     return output_fname
 
 
+def process_dropdown_select(inp: gr.SelectData):
+    # print(inp.selected,inp.target,inp.value,inp.index)
+    imgs = [f"./heads/{i}" for i in os.listdir("./heads")
+            if any(i.endswith(suffix) for suffix in ["jpeg", "png", "jpg"])]
+    return gr.update(choices=imgs), inp.value
+
+
 # 创建Gradio界面
 # 控制图片的比例
 with gr.Blocks() as demo:
     gr.Markdown("图像处理界面")
     with gr.Row():
-        default_head = np.array(Image.open("head.jpeg")) if os.path.exists("head.jpeg") else None
-        head_image = gr.Image(label="上传 'head.jpeg' 图像", value=default_head).style(height=400)
-        target_image_fp = gr.Image(label="上传 'target.jpeg' 图像", type="filepath").style(height=400)
-        target_video = gr.Video(label="上传视频").style(height=400)
-    enhancer_checkbox = gr.Checkbox(label="启用 enhancer(面部占比大的时候——自拍、怼脸特写，建议启用)")
+        # 注意Image的type="filepath"时，拿到的是一个临时路径，basename都是image.png而不是原始文件名
+        # 只有Video拿到的是上传文件的原始名称
+        head_image_fp = gr.Image(label="上传 'head.jpeg' 图像", type="filepath").style(height=300)
+        target_image_fp = gr.Image(label="上传 'target.jpeg' 图像", type="filepath").style(height=300)
+        target_video = gr.Video(label="上传视频").style(height=300)
+    with gr.Row():
+        img_choices = [f"./heads/{i}" for i in os.listdir("./heads")
+                       if any(i.endswith(suffix) for suffix in ["jpeg", "png", "jpg"])]
+        dropdown = gr.Dropdown(choices=img_choices, label="select heads")
+        enhancer_checkbox = gr.Checkbox(label="启用 enhancer(面部占比大的时候——自拍、怼脸特写，建议启用)")
     with gr.Row():
         btn_image = gr.Button("处理图像")
         btn_video = gr.Button("处理视频")
     with gr.Row():
-        output_image = gr.Image(label="处理后的图像").style(height=400)
-        output_video = gr.Video(label="处理后的视频").style(height=400)
+        output_image = gr.Image(label="处理后的图像", type="filepath").style(height=300)
+        output_video = gr.Video(label="处理后的视频").style(height=300)
 
+    # Listeners
     btn_image.click(process_images,
-                    inputs=[head_image, target_image_fp, enhancer_checkbox],
+                    inputs=[dropdown, target_image_fp, enhancer_checkbox],
                     outputs=output_image)
 
     btn_video.click(process_videos,
-                    inputs=[head_image, target_video, enhancer_checkbox],
+                    inputs=[dropdown, target_video, enhancer_checkbox],
                     outputs=output_video)
+
+    dropdown.select(process_dropdown_select, outputs=[dropdown, head_image_fp])
 
 # 启动Gradio界面
 demo.launch(server_port=6006)
