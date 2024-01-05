496a8f176e33f638cbadcd021459486ffc1eefdd
Zachary4biz <zachary4biz@yeah.net>
2024-01-06 04:36:32 +0800

    update

--- run_multi_img.sh
+++ run_multi_img.sh
@@ -7,12 +7,13 @@ for file in "$1"/*; do
     # 检查是否为文件
     if [ -f "$file" ]; then
         # 获取文件名（不带扩展名）
-        filename="${file%.*}"
-        # 获取文件扩展名
-        extension="${file##*.}"
-        # 构建输出文件名
-        output="${filename}_${NAME}_swap.${extension}"
-        echo "S_IMG: ${S_IMG} file: ${file} output: ${output}"
+        f_dirname=$(dirname $file)
+        f_basename=$(basename $file)
+        output="${f_dirname}/${NAME}_${f_basename}"
+        echo "f_dirname: ${f_dirname}"
+        echo "f_basename: ${f_basename}"
+        echo "output: ${output}"
+
         # 执行 python 命令
         python run.py --execution-provider cuda -s $S_IMG -t ${file} -o ${output} --frame-processor face_swapper --execution-threads 20
     fi
