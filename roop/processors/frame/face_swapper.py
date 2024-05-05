eec01e1448213d492448aa19063bd2f0c0deaa13
zach <zachx121@outlook.com>
2024-05-06 04:31:23 +0800

    skip frame test2

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -96,7 +96,7 @@ def process_frames(source_path: str, temp_frame_paths: List[str], update: Callab
             update()
 
     # 未实现替换的帧剔除掉
-    print("未完成替换的帧: %s" % " ".join([os.path.basename(p) for p in nonswap_frame_paths]))
+    print("未完成替换的帧(%s/%s): %s" % (len(nonswap_frame_paths), len(temp_frame_paths), " ".join([os.path.basename(p) for p in nonswap_frame_paths])))
     if roop.globals.skip_nonswap_frame and len(nonswap_frame_paths) > 0:
         # print(f">>> 未实现替换的帧剔除掉 (e.g. {nonswap_frame_paths[0]})")
         for p in nonswap_frame_paths:
